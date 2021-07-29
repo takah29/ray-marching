c76454f417cc87465a48e1b2c9ce78fd4f2383f8
takah29 <takah0209@gmail.com>
2021-07-30 01:19:33 +0900

    シーンの修正

--- main.glsl
+++ main.glsl
@@ -1,10 +1,11 @@
 precision mediump float;
 uniform float time;
-uniform vec2  mouse;
-uniform vec2  resolution;
+uniform vec2 mouse;
+uniform vec2 resolution;
 
-#include "object/sphere.glsl"
 #include "object/box.glsl"
+#include "object/plane.glsl"
+#include "object/sphere.glsl"
 
 const float PI = 3.14159265;
 const float angle = 60.0;
@@ -12,44 +13,65 @@ const float fov = angle * 0.5 * PI / 180.0;
 
 const vec3 lightDir = vec3(-0.577, 0.577, 0.577);
 
-vec3 trans(in vec3 p, in float scale){
-    return mod(p, scale) - scale / 2.0;
+vec3 trans(in vec3 p, in float scale) { return mod(p, scale) - scale / 2.0; }
+
+// オブジェクト
+Sphere sphere = Sphere(vec3(0.0, 0.0, 0.0), 1.0);
+Box box = Box(vec3(0.0, 0.0, 0.0), vec3(0.5, 0.5, 0.5), 0.1);
+Plane plane = Plane(vec3(0.0, -2.0, 0.0), vec3(0.0, 1.0, 0.0));
+
+float distance_scene(in vec3 p) {
+    float d1 = distance_func(sphere, trans(p, 4.0));
+    float d2 = distance_func(plane, p);
+    return min(d1, d2);
 }
 
-void main(void){
+vec3 get_normal(in vec3 p) {
+    float d = 0.0001;
+    return normalize(vec3(distance_scene(p + vec3(d, 0.0, 0.0)) - distance_scene(p + vec3(-d, 0.0, 0.0)),
+                          distance_scene(p + vec3(0.0, d, 0.0)) - distance_scene(p + vec3(0.0, -d, 0.0)),
+                          distance_scene(p + vec3(0.0, 0.0, d)) - distance_scene(p + vec3(0.0, 0.0, -d))));
+}
+
+vec3 ray_march(vec3 p, in vec3 ray) {
+    float distance = 0.0;  // レイとオブジェクト間の最短距離
+    float len = 0.0;       // レイに継ぎ足す長さ
+    vec3 pos = p;          // レイの先端位置
+    vec3 color;
+    // marching loop
+    for (int i = 0; i < 128; i++) {
+        distance = distance_scene(pos);
+        len += distance;
+        pos = p + ray * len;
+
+        // hit check
+        if (distance < 0.001) {
+            vec3 normal = get_normal(pos);
+            float diff = clamp(dot(lightDir, normal), 0.1, 1.0);
+            color = vec3(diff);
+            break;
+        } else {
+            color = vec3(0.0);
+        }
+    }
+
+    return color;
+}
+
+void main(void) {
     // fragment position
     vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
 
-    // オブジェクト
-    Sphere sphere = Sphere(vec3(0.0, 0.0, 0.0), 1.0);
-    Box box = Box(vec3(0.0, 0.0,0.0), vec3(0.5, 0.5, 0.5), 0.1);
-
     // camera
-    vec3 cPos = vec3(mouse * 4.0 - 2.0,  2.0);
-    vec3 cDir = vec3(0.0,  0.0, -1.0);
-    vec3 cUp  = vec3(0.0,  1.0,  0.0);
-    vec3 cSide = cross(cDir, cUp);
-    float targetDepth = 1.0;
+    vec3 c_pos = vec3(mouse * 4.0 - 2.0, 2.0);
+    vec3 c_dir = vec3(0.0, 0.0, -1.0);
+    // vec3 c_up = vec3(0.0    , 1.0, 0.0);
+    // vec3 c_side = cross(c_dir, c_up);
+    // float targetDepth = 1.0;
 
     // ray
     vec3 ray = normalize(vec3(sin(fov) * p.x, sin(fov) * p.y, -cos(fov)));
 
-    // marching loop
-    float distance = 0.0; // レイとオブジェクト間の最短距離
-    float rLen = 0.0;     // レイに継ぎ足す長さ
-    vec3  rPos = cPos;    // レイの先端位置
-    for(int i = 0; i < 640; i++){
-        distance = distance_func(box, trans(rPos, 4.0));
-        rLen += distance;
-        rPos = cPos + ray * rLen;
-    }
-
-    // hit check
-    if(abs(distance) < 0.001){
-        vec3 normal = get_normal(box, trans(rPos, 4.0));
-        float diff = clamp(dot(lightDir, normal), 0.1, 1.0);
-        gl_FragColor = vec4(vec3(diff), 1.0);
-    }else{
-        gl_FragColor = vec4(vec3(0.0), 1.0);
-    }
+    vec3 color = ray_march(c_pos, ray);
+    gl_FragColor = vec4(color, 1.0);
 }
