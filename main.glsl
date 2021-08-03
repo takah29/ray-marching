56d6f96581559c5887b1863fbbc77b4ccf3d5eec
takah29 <takah0209@gmail.com>
2021-08-04 01:49:32 +0900

    シーンの更新

--- main.glsl
+++ main.glsl
@@ -1,5 +1,4 @@
-#iUniform int n = 10 in{10, 1280 }
-#iUniform float d = 0.01 in {0.01, 1.0}
+#iUniform float u = 1.0 in{0.0, 10.0 }
 
 precision mediump float;
 uniform float time;
@@ -7,46 +6,55 @@ uniform vec2 mouse;
 uniform vec2 resolution;
 
 #include "object.glsl"
-#include "transform.glsl"
 #include "operation.glsl"
+#include "transform.glsl"
 
 const float PI = 3.14159265;
 const float angle = 60.0;
 const float fov = angle * 0.5 * PI / 180.0;
 
-const vec3 lightDir = vec3(-0.577, 0.577, 0.577);
+const vec3 light = vec3(0.577, 0.477, 0.177);
 
 // オブジェクト
-Sphere sphere = Sphere(vec3(0.0, 0.0, 0.0), 1.0);
-Box box = Box(vec3(0.0, 0.0, 0.0), vec3(0.5, 0.5, 0.5), 0.1);
-Torus torus = Torus(vec3(0.0, 1.0, -10.0), 10.0, 3.0);
-Plane plane = Plane(vec3(0.0, -0.5, 0.0), vec3(0.0, 1.0, 0.0));
-
-
-// float distance_scene(vec3 p)
-// {
-//     float t = time;
-//     float a = PI * t;
-//     float s = pow(sin(a), 2.0);
-//     float d1 = distance_func(Sphere(vec3(0.0, 0.0, 0.0), 0.75), p);
-//     float d2 = distance_func(Sphere(vec3(0.0), 0.1), trans(p, 0.5));
-//     return mix(d1, d2, s);
-// }
+Sphere sphere = Sphere(vec3(0.0, 0.0, 0.0), 3.0);
+Box box = Box(vec3(0.0, 0.0, 0.0), vec3(0.07, 0.07, 0.07), 0.04);
+Torus torus = Torus(vec3(0.0, 0.0, 0.0), 2.0, 1.0);
+Plane plane = Plane(vec3(0.0, -2.0, 0.0), vec3(0.0, 1.0, 0.0));
 
 float distance_scene(in vec3 p) {
-    float d1 = distance_func(sphere, repetition(p, vec3(0.0), vec3(4.0)));
-    float d2 = distance_func(torus, p);
+    vec3 q = rotate_z(rotate_x(rotate_y(p, time * 0.5), time * 0.2), 0.1);
+    float d1 = distance_func(box, repetition(q, vec3(0.0), vec3(0.4)));
+    torus.radius_a = u;
+    float d2 = distance_func(torus, q);
+
     float d3 = distance_func(plane, p);
 
-    float d = mix(d1, d2, pow(sin(time), 2.0));
-    return op_union(d, d3);
+    float d = mix(smooth_intersection(d1, d2, 0.5), d2, pow(sin(time * 0.5), 2.0));
+    return smooth_union(d, d3, 0.5);
 }
 
+// シーンの法線ベクトルの計算
 vec3 get_normal(in vec3 p) {
     float d = 0.0001;
     return normalize(vec3(distance_scene(p + vec3(d, 0.0, 0.0)) - distance_scene(p + vec3(-d, 0.0, 0.0)),
                           distance_scene(p + vec3(0.0, d, 0.0)) - distance_scene(p + vec3(0.0, -d, 0.0)),
                           distance_scene(p + vec3(0.0, 0.0, d)) - distance_scene(p + vec3(0.0, 0.0, -d))));
+}
+
+float gen_shadow(vec3 ro, vec3 rd) {
+    float h = 0.0;
+    float c = 0.001;
+    float r = 1.0;
+    float shadow_coef = 0.5;
+    for (int i = 0; i < 128; i++) {
+        h = distance_scene(ro + rd * c);
+        if (abs(h) < 0.001) {
+            return shadow_coef;
+        }
+        r = min(r, h * 32.0 / c);
+        c += h;
+    }
+    return 1.0 - shadow_coef + r * shadow_coef;
 }
 
 vec3 ray_march(vec3 p, in vec3 ray) {
@@ -56,21 +64,30 @@ vec3 ray_march(vec3 p, in vec3 ray) {
     vec3 color = vec3(0.0);
 
     // marching loop
-    for (int i = 0; i < n; i++) {
+    float shadow = 1.0;
+    for (int i = 0; i < 256; i++) {
         distance = distance_scene(pos);
         len += distance;
         pos = p + ray * len;
 
         // hit check
-        if (distance < 0.001) {
+        if (abs(distance) < 0.001) {
             vec3 normal = get_normal(pos);
-            float diff = clamp(dot(lightDir, normal), 0.1, 1.0);
-            color = vec3(diff);
+
+            // light
+            vec3 halfLE = normalize(light - ray);
+            float diff = clamp(dot(light, normal), 0.1, 1.0);
+            float spec = pow(clamp(dot(halfLE, normal), 0.0, 1.0), 50.0);
+
+            // generate shadow
+            shadow = gen_shadow(pos + normal * 0.001, light);
+
+            color = vec3(diff) + vec3(spec);
             break;
         }
     }
 
-    return color;
+    return color * max(0.5, shadow);
 }
 
 void main(void) {
@@ -78,11 +95,7 @@ void main(void) {
     vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
 
     // camera
-    vec3 c_pos = vec3(mouse * 4.0 - vec2(2.0, -10.0), 16.0);
-    vec3 c_dir = vec3(0.0, -1.0, -0.0);
-    // vec3 c_up = vec3(0.0    , 1.0, 0.0);
-    // vec3 c_side = cross(c_dir, c_up);
-    // float targetDepth = 1.0;
+    vec3 c_pos = vec3(0.0, 0.0, 8.0);
 
     // ray
     vec3 ray = normalize(vec3(sin(fov) * p.x, sin(fov) * p.y, -cos(fov)));
