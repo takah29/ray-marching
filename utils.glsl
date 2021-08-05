92cfa4ead0f1050fd496de981f46988efbee6522
takah29 <takah0209@gmail.com>
2021-08-06 04:00:43 +0900

    マテリアルの表現のためのデータ構造を追加

---
+++ utils.glsl
@@ -0,0 +1,15 @@
+vec3 RED = vec3(1.0, 0.0, 0.0);
+vec3 GREEN = vec3(0.0, 1.0, 0.0);
+vec3 BLUE = vec3(0.0, 0.0, 1.0);
+vec3 CYAN = vec3(0.0, 1.0, 1.0);
+vec3 MAGENTA = vec3(1.0, 0.0, 1.0);
+vec3 YELLOW = vec3(1.0, 1.0, 0.0);
+vec3 WHITE = vec3(1.0, 1.0, 1.0);
+vec3 BLACK = vec3(0.0, 0.0, 0.0);
+
+struct HitPoint {
+    float d;   // distance
+    vec4 mtl;  // RGB, Speculer
+};
+
+vec4 blend_mtl(in vec4 mtl1, in vec4 mtl2, in float t) { return (1.0 - t) * mtl1 + t * mtl2; }
