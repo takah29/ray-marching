212c2f6b1a9bda199b3b3c1a7cb818989de2fcbc
takah29 <takah0209@gmail.com>
2021-08-06 04:02:30 +0900

    シーンの更新

--- main.glsl
+++ main.glsl
@@ -17,11 +17,11 @@ const vec3 light = vec3(0.577, 0.477, 0.177);
 
 // オブジェクト
 Sphere sphere = Sphere(vec3(0.0, 0.0, 0.0), 3.0);
-Box box = Box(vec3(0.0, 0.0, 0.0), vec3(0.07, 0.07, 0.07), 0.04);
+Box box = Box(vec3(0.0, 0.0, 0.0), vec3(0.06, 0.06, 0.06), 0.06);
 Torus torus = Torus(vec3(0.0, 0.0, 0.0), 2.0, 1.0);
 Plane plane = Plane(vec3(0.0, -2.0, 0.0), vec3(0.0, 1.0, 0.0));
 
-float distance_scene(in vec3 p) {
+HitPoint distance_scene(in vec3 p) {
     vec3 q = rotate_z(rotate_x(rotate_y(p, time * 0.5), time * 0.2), 0.1);
     float d1 = distance_func(box, repetition(q, vec3(0.0), vec3(0.4)));
     torus.radius_a = u;
@@ -29,16 +29,16 @@ float distance_scene(in vec3 p) {
 
     float d3 = distance_func(plane, p);
 
-    float d = mix(smooth_intersection(d1, d2, 0.5), d2, pow(sin(time * 0.5), 2.0));
-    return smooth_union(d, d3, 0.5);
+    float d = mix(d2, d1, pow(sin(time * 1.0) * 0.99, 2.0));
+    return smooth_union(HitPoint(d, vec4(BLUE, 1.0)), HitPoint(d3, vec4(GREEN, 0.0)), 0.5);
 }
 
 // シーンの法線ベクトルの計算
 vec3 get_normal(in vec3 p) {
     float d = 0.0001;
-    return normalize(vec3(distance_scene(p + vec3(d, 0.0, 0.0)) - distance_scene(p + vec3(-d, 0.0, 0.0)),
-                          distance_scene(p + vec3(0.0, d, 0.0)) - distance_scene(p + vec3(0.0, -d, 0.0)),
-                          distance_scene(p + vec3(0.0, 0.0, d)) - distance_scene(p + vec3(0.0, 0.0, -d))));
+    return normalize(vec3(distance_scene(p + vec3(d, 0.0, 0.0)).d - distance_scene(p + vec3(-d, 0.0, 0.0)).d,
+                          distance_scene(p + vec3(0.0, d, 0.0)).d - distance_scene(p + vec3(0.0, -d, 0.0)).d,
+                          distance_scene(p + vec3(0.0, 0.0, d)).d - distance_scene(p + vec3(0.0, 0.0, -d)).d));
 }
 
 float gen_shadow(vec3 ro, vec3 rd) {
@@ -46,8 +46,8 @@ float gen_shadow(vec3 ro, vec3 rd) {
     float c = 0.001;
     float r = 1.0;
     float shadow_coef = 0.5;
-    for (int i = 0; i < 128; i++) {
-        h = distance_scene(ro + rd * c);
+    for (int i = 0; i < 64; i++) {
+        h = distance_scene(ro + rd * c).d;
         if (abs(h) < 0.001) {
             return shadow_coef;
         }
@@ -65,21 +65,22 @@ vec3 ray_march(vec3 p, in vec3 ray) {
 
     // marching loop
     float shadow = 1.0;
+    HitPoint hp;
     for (int i = 0; i < 256; i++) {
-        distance = distance_scene(pos);
-        len += distance;
+        hp = distance_scene(pos);
+        len += hp.d;
         pos = p + ray * len;
 
         // hit check
-        if (abs(distance) < 0.001) {
+        if (abs(hp.d) < 0.001) {
             vec3 normal = get_normal(pos);
 
             // light
             vec3 halfLE = normalize(light - ray);
-            float diff = clamp(dot(light, normal), 0.1, 1.0);
-            float spec = pow(clamp(dot(halfLE, normal), 0.0, 1.0), 50.0);
+            vec3 diff = clamp(dot(light, normal), 0.1, 1.0) * hp.mtl.xyz;
+            float spec = pow(clamp(dot(halfLE, normal), 0.0, 1.0), 50.0) * hp.mtl.w;
 
-            // generate shadow
+            // shadow
             shadow = gen_shadow(pos + normal * 0.001, light);
 
             color = vec3(diff) + vec3(spec);
